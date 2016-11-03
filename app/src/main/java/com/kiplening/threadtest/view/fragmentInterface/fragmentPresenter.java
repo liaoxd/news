04b126fb0a396996b2ba6154499c310c6f561fbb
MOON <18384217426@163.com>
2016-11-03 21:43:38 +0800

    开始重构。。。

---
+++ app/src/main/java/com/kiplening/threadtest/view/fragmentInterface/fragmentPresenter.java
@@ -0,0 +1,11 @@
+package com.kiplening.threadtest.view.fragmentInterface;
+
+/**
+ * Created by MOON on 11/3/2016.
+ */
+
+public interface fragmentPresenter {
+    public void onSuccess();
+    public void onFailure();
+    public void onFinish();
+}
