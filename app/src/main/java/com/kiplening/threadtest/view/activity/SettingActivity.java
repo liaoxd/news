46265aa4ac0e6701d4446d249f684430ac37587f
MOON <18384217426@163.com>
2016-11-16 20:38:33 +0800

    重构之路。。。

---
+++ app/src/main/java/com/kiplening/threadtest/view/activity/SettingActivity.java
@@ -0,0 +1,25 @@
+package com.kiplening.threadtest.view.activity;
+
+import android.os.Bundle;
+
+import com.example.mylibrary.activity.BaseActivity;
+
+/**
+ * Created by MOON on 11/16/2016.
+ */
+public class SettingActivity extends BaseActivity{
+    @Override
+    protected void initVariables() {
+
+    }
+
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        //setContentView();
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
